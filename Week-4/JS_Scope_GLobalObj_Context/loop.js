af22d5f7fc2ff1d4348bf66a718bf7ffa93e0b94
Anjani-22 <anjaniofficial22@gmail.com>
2023-08-28 14:32:36 +0530

    loop first draft

---
+++ Week-4/JS_Scope_GLobalObj_Context/loop.js
@@ -0,0 +1,12 @@
+function drawTraingle(num)
+{
+    for(i=1;i<=num;i++)
+    {
+        var s="";
+        for(j=1;j<=i;j++)
+        {
+            s+="* ";
+        }
+        console.log(s);
+    }
+}
