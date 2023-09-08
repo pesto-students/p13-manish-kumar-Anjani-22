801713ad91b7b10638d7a5fe1075086621c14ef3
Anjani-22 <anjaniofficial22@gmail.com>
2023-09-08 12:05:43 +0530

    Symbol completed

--- Week-5/Advance_JS_Managing_Objects/Symbols.js
+++ Week-5/Advance_JS_Managing_Objects/Symbols.js
@@ -1,9 +1,11 @@
-let index=0;
-function generateTransactionId()
-{ index++;
-   const id = "TRANSACTION_ID_" + index;
-   const TRANSACTION_ID=Symbol(id);
-   return TRANSACTION_ID;
+function generateTransactionId() {
+  let index = 0;
+  return function () {
+    index++;
+    const id = "TRANSACTION_ID_" + index;
+    const TRANSACTION_ID = Symbol(id);
+    return TRANSACTION_ID;
+  };
 }
 
 console.log(generateTransactionId());
