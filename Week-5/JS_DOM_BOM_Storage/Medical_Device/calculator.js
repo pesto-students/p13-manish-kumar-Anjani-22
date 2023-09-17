edb425af8c558138eccd98eb6f9b7c160e418e7a
Anjani-22 <anjaniofficial22@gmail.com>
2023-09-17 22:22:56 +0530

    updateScore.js started

--- Week-5/JS_DOM_BOM_Storage/Medical_Device/calculator.js
+++ Week-5/JS_DOM_BOM_Storage/Medical_Device/calculator.js
@@ -1,6 +1,35 @@
-document.addEventListener("DOMContentLoaded", updateScore);
+document.querySelectorAll('input[type="radio"]').forEach(function (radio) {
+  radio.addEventListener("click", updateScore);
+});
 
 function updateScore() {
-  const radioButtons = document.querySelectorAll("input[type='radio']");
-  const warningLabel = document.getElementById("warning");
+  const radioButtonClassNames = [
+    "AV",
+    "AC",
+    "PR",
+    "UI",
+    "scope",
+    "integ",
+    "avail",
+    "conf",
+    "health",
+    "sense",
+  ];
+  let allRadioButtonSelected = true;
+
+  let selectedRadioButtons = [];
+  for (let radioClass of radioButtonClassNames) {
+    let radioGroup = document.getElementsByClassName(radioClass);
+    let radioSelected = radioGroup.filter((radioButton) => radioButton.checked);
+    if (radioSelected.length == 0) {
+      allRadioButtonSelected = false;
+    } else {
+      selectedRadioButtons.push(radioSelected[0].value);
+    }
+  }
+  if (allRadioButtonSelected) {
+    document.getElementById("warning").style.display = "none";
+    for (let radioValue of selectedRadioButtons) {
+    }
+  }
 }
