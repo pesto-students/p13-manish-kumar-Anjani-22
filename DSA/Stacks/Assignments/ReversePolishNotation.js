e85e39e4dbb1f458e20d1670da96365552b05b86
Anjani-22 <anjaniofficial22@gmail.com>
2023-10-24 23:39:26 +0530

    ReversePolishNotation

---
+++ DSA/Stacks/Assignments/ReversePolishNotation.js
@@ -0,0 +1,42 @@
+import Stack from "./Stacks/StackArr.js";
+
+let input = prompt(
+  " Enter an array of tokens in RPN format seperated by space"
+);
+
+reversePolishNotation(input);
+
+// similar to post fix expression
+function reversePolishNotation(input) {
+  let rpn = input.split(" ");
+  let stack = new Stack();
+  let operator = "+-/%*";
+  for (let i = 0; i < rpn.lenght; i++) {
+    let element = rpn[i];
+    if (isNaN(element)) {
+      if (operator.includes(element)) {
+        let oper = element;
+        let first = Number(stack.pop());
+        let second = Number(stack.pop());
+        let result = performOperation(oper, first, second);
+        stack.push(result);
+      }
+    } else {
+      stack.push(element);
+    }
+  }
+}
+function performOperation(operator, first, second) {
+  switch (operator) {
+    case "+":
+      return first + second;
+    case "-":
+      return first - second;
+    case "*":
+      return first * second;
+    case "/":
+      return first / second;
+    case "%":
+      return first % second;
+  }
+}
