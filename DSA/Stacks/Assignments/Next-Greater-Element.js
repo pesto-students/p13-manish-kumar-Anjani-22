eb0132e0f19e24107b101d6ab8f64b04773ef6ab
Anjani-22 <anjaniofficial22@gmail.com>
2023-10-24 23:12:50 +0530

    updated indexOf() to move to next element in nge

--- DSA/Stacks/Assignments/Next-Greater-Element.js
+++ DSA/Stacks/Assignments/Next-Greater-Element.js
@@ -1,21 +1,25 @@
 import Stack from "./Stacks/StackLL.js";
 function nextGreaterElement(arr) {
+  console.log(arr);
   let nge = new Array(arr.length).fill(-1);
   let stack = new Stack();
   for (let i = 0; i < arr.length; i++) {
     let element = arr[i];
 
     while (!stack.isEmpty() && element > stack.peek()) {
-      let index = arr.indexOf(stack.pop());
+      let top = stack.pop();
+      let index = arr.indexOf(top);
+
+      while (nge[index] != -1) {
+        index = arr.indexOf(top, index);
+      }
+
       nge[index] = element;
     }
     stack.push(element);
   }
 
-  while (!stack.isEmpty()) {
-    let index = arr.indexOf(stack.pop());
-    nge[index] = -1;
-  }
+  while (!stack.isEmpty()) stack.pop();
 
   console.log(nge);
 }
