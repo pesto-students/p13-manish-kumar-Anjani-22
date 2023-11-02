0278b06abcd9de818d822562da695b2223e7b0bb
Anjani-22 <anjaniofficial22@gmail.com>
2023-11-02 11:47:40 +0530

    LevelOrderTraversalIndexbased

--- DSA/Trees/Mirror.js
+++
@@ -1,9 +0,0 @@
-// function symmetry(node) {
-//   if (!node.left && node.right) return false;
-//   if (node.left && !node.right) return false;
-//   if (!node.left && !node.right) return right;
-
-//   let left = node.left;
-//   let right = node.right;
-//   return left.value === right.value && symmetry(left) && symmetry(right);
-// }
