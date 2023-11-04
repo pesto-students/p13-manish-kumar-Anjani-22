439a6bf2a55830b9e6f52a5be685e9d95f00a273
Anjani-22 <anjaniofficial22@gmail.com>
2023-11-04 22:00:40 +0530

    removed console.log from app.get()

--- Projects/Node-Backend/3.1 Express Server/index.js
+++ Projects/Node-Backend/3.1 Express Server/index.js
@@ -9,9 +9,6 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.send(` hello user again ${req.rawHeaders}`);
-  console.log(`reques: ${req}`);
-  console.log("\n\n\n");
-  console.log(`respons: ${res.json}`);
 });
 app.get("/endpoint1", (req, res) => {
   res.send("<h1>endpoint1</h1>");
