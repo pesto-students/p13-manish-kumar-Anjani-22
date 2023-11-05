c06de23774809a71894885414948b8a9c189393f
Anjani-22 <anjaniofficial22@gmail.com>
2023-11-05 13:58:26 +0530

    cmt added for self code of BST to LL

--- DSA/BST/BST-LL.js
+++ DSA/BST/BST-LL.js
@@ -5,3 +5,20 @@ function bst_toLinkedList(BSTNode, LL) {
   LL.insert(BSTNode.value);
   if (!BSTNode.left) bst_toLinkedList(BSTNode.right, LL);
 }
+
+/*  if (!BSTNode.left && !BSTNode.right) return BSTNode.value;
+  if (!BSTNode.left) {
+    LL.insert(BSTNode.value);
+    bst_toLinkedList(BSTNode.right, LL);
+    return bst_toLinkedList(BSTNode.right, LL);
+  } else {
+    leftValue = bst_toLinkedList(BSTNode);
+    LL.insert(leftValue);
+
+    LL.insert(BSTNode.value);
+
+    rightValue = bst_toLinkedList(BSTNode.right, LL);
+    LL.insert(rightValue);
+    if (!rightValue) LL.insert(rightValue);
+  }
+}*/
