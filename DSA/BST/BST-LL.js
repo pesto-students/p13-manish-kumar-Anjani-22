cb97f83396bc248448290ac5a249db42f28ffe6d
Anjani-22 <anjaniofficial22@gmail.com>
2023-11-05 13:28:25 +0530

    modified:   DSA/BST/BST-LL.js

--- DSA/BST/BST-LL.js
+++ DSA/BST/BST-LL.js
@@ -1,17 +1,7 @@
 function bst_toLinkedList(BSTNode, LL) {
   //LLNode null
-  if (!node) return;
-  if (!BSTNode.left && !BSTNode.right) return BSTNode.value;
-  if (!BSTNode.left) {
-    LL.insert(BSTNode.value);
-    return bst_toLinkedList(BSTNode.right, LL);
-  } else {
-    leftValue = bst_toLinkedList(BSTNode);
-    LL.insert(leftValue);
-
-    LL.insert(BSTNode.value);
-
-    rightValue = bst_toLinkedList(BSTNode.right, LL);
-    if (!rightValue) LL.insert(rightValue);
-  }
+  if (!BSTNode) return;
+  if (!BSTNode.left) bst_toLinkedList(BSTNode.left, LL);
+  LL.insert(BSTNode.value);
+  if (!BSTNode.left) bst_toLinkedList(BSTNode.right, LL);
 }
