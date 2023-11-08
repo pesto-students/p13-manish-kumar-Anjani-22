b1234e7595264b5f13911d26c608019b2250889e
Anjani-22 <102584599+Anjani-22@users.noreply.github.com>
2023-11-08 13:56:26 +0530

    Create LastOccurance.js

---
+++ DSA/BinarySearch/LastOccurance.js
@@ -0,0 +1,13 @@
+function lastOccurance(arr, target) {
+  return helper(arr, 0, arr.length, target);
+}
+
+function helper(arr, l, h, target) {
+  const mid = Math.floor(l + h - h / 2);
+  if (arr[mid] < target) return helper(arr, 0, mid - 1, target);
+  else if (arr[mid] > target) return helper(arr, mid + 1, h, target);
+  else {
+    if (mid === arr.length - 1 || arr[mid + 1] > target) return mid;
+    else return helper(arr, mid + 1, h, target);
+  }
+}
