c635297693d4337ad2e914506ff8ae67702493ad
Anjani-22 <anjaniofficial22@gmail.com>
2023-11-27 18:28:37 +0530

    return index

--- DSA/BinarySearch-2/FindPeak.js
+++ DSA/BinarySearch-2/FindPeak.js
@@ -2,8 +2,8 @@ function findPeak(arr, left, right) {
   if (left > right) return;
   const mid = Math.floor(left + (right - left / 2));
 
-  if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) return arr[mid];
-  else if (mid === arr.lenghth) return arr[mid];
+  if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) return mid;
+  else if (mid === arr.lenghth) return mid;
   else if (arr[mid] < arr[mid + 1]) {
     findPeak(arr, mid + 1, right);
   } else {
